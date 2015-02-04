a44ce00b39807a9411dc390b8d98f2cd301d69a3
Bugra <bugra.bilin@cern.ch>
2015-02-04 17:35:54 +0100

    running on CMSSW_7_2_0

--- Tupel/run_cfg.py
+++ Tupel/run_cfg.py
@@ -7,42 +7,63 @@ process.source = cms.Source("PoolSource",
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
+from RecoJets.JetProducers.ak4PFJets_cfi import ak4PFJets
 from RecoJets.JetProducers.ak5GenJets_cfi import ak5GenJets
-from RecoMET.METProducers.PFMET_cfi import pfMet
+from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
+#from RecoMET.METProducers.PFMET_cfi import pfMet
 
 process.chs = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV"))
 
-process.ak5PFJets = ak5PFJets.clone(src = 'packedPFCandidates', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
+#process.ak5PFJets = ak5PFJets.clone(src = 'packedPFCandidates', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
 process.ak5PFJetsCHS = ak5PFJets.clone(src = 'chs', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
+process.ak4PFJetsCHS = ak4PFJets.clone(src = 'chs', doAreaFastjet = True)
 process.ak5GenJets = ak5GenJets.clone(src = 'packedGenParticles')
-process.pfMet = pfMet.clone(src = "packedPFCandidates")
-process.pfMet.calculateSignificance = False # this can't be easily implemented on packed PF candidates at the moment
+process.ak4GenJets = ak5GenJets.clone(src = 'packedGenParticles')
+#process.pfMet = pfMet.clone(src = "packedPFCandidates")
+#process.pfMet.calculateSignificance = False # this can't be easily implemented on packed PF candidates at the moment
 
 process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
 process.load("Configuration.EventContent.EventContent_cff")
 process.load('Configuration.StandardSequences.Geometry_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'START70_V6::All'
+process.GlobalTag.globaltag = 'PHYS14_25_V1::All'
 
 
-
+process.load("RecoJets.JetAssociationProducers.ak5JTA_cff")
+process.load("RecoJets.JetAssociationProducers.ak4JTA_cff")
 process.patJets.addJetCharge   = False
 process.patJets.addBTagInfo    = True
 process.patJets.getJetMCFlavour = False
 process.patJets.addAssociatedTracks = False
-process.patJetPartonMatch.matched = "prunedGenParticles"
-process.patJetCorrFactors.primaryVertices = "offlineSlimmedPrimaryVertices"
-process.patMETs.addGenMET = False # There's no point in recalculating this, and we can't remake it since we don't have genParticles beyond |eta|=5
+#process.patJetPartonMatch.matched = "prunedGenParticles"
+#process.patJetCorrFactors.primaryVertices = "offlineSlimmedPrimaryVertices"
+#process.patMETs.addGenMET = False # There's no point in recalculating this, and we can't remake it since we don't have genParticles beyond |eta|=5
 
 process.load('RecoBTag.Configuration.RecoBTag_cff')
-process.load('RecoJets.Configuration.RecoJetAssociations_cff')
+#process.load('RecoJets.Configuration.RecoJetAssociations_cff')
 
 process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
 
 process.ak5JetTracksAssociatorAtVertexPF.jets = cms.InputTag("ak5PFJetsCHS")
 process.ak5JetTracksAssociatorAtVertexPF.tracks = cms.InputTag("unpackedTracksAndVertices")
+process.ak4JetTracksAssociatorAtVertexPF.jets = cms.InputTag("ak4PFJetsCHS")
+process.ak4JetTracksAssociatorAtVertexPF.tracks = cms.InputTag("unpackedTracksAndVertices")
 process.impactParameterTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
+#process.pfImpactParameterTagInfos.primaryVertex=cms.InputTag("unpackedTracksAndVertices")
+
+process.patJets.discriminatorSources = cms.VInputTag(cms.InputTag("jetBProbabilityBJetTags"),
+ cms.InputTag("jetProbabilityBJetTags"), 
+cms.InputTag("trackCountingHighPurBJetTags"),
+ cms.InputTag("trackCountingHighEffBJetTags"), 
+cms.InputTag("simpleSecondaryVertexHighEffBJetTags"), 
+cms.InputTag("simpleSecondaryVertexHighPurBJetTags"),
+ cms.InputTag("combinedInclusiveSecondaryVertexV2BJetTags"),
+ cms.InputTag("combinedSecondaryVertexBJetTags")
+#,cms.InputTag("combinedMVABJetTags")
+) 
+
+
 process.inclusiveSecondaryVertexFinderTagInfos.extSVCollection = cms.InputTag("unpackedTracksAndVertices","secondary","")
 process.combinedSecondaryVertex.trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt
 
@@ -65,6 +86,11 @@ process.TFileService = cms.Service("TFileService",
 #     pfIsolationVariables().sumPhotonEt-
 #     0.5*pfIsolationVariables().sumPUPt))/pt < 0.15'''))
  
+process.patJetPartons.particles=cms.InputTag("prunedGenParticles")
+process.patJetPartonMatch.matched = cms.InputTag("prunedGenParticles")
+process.patJetCorrFactors.primaryVertices=cms.InputTag("offlineSlimmedPrimaryVertices")
+
+
 
 process.tupel = cms.EDAnalyzer("Tupel",
 #  trigger      = cms.InputTag( "patTrigger" ),
@@ -77,12 +103,15 @@ process.tupel = cms.EDAnalyzer("Tupel",
   jetSrc      = cms.untracked.InputTag("slimmedJets"),
   metSrc      = cms.untracked.InputTag("patMETsPF"),
   genSrc      = cms.untracked.InputTag("prunedGenParticles"),
+  gjetSrc       = cms.untracked.InputTag('slimmedGenJets'),
   muonMatch    = cms.string( 'muonTriggerMatchHLTMuons' ),
   muonMatch2    = cms.string( 'muonTriggerMatchHLTMuons2' ),
   elecMatch    = cms.string( 'elecTriggerMatchHLTElecs' ),
   mSrcRho      = cms.untracked.InputTag('fixedGridRhoFastjetAll'),#arbitrary rho now
   CalojetLabel = cms.untracked.InputTag('slimmedJets'), #same collection now BB 
-  metSource = cms.VInputTag("slimmedMETs","slimmedMETs","slimmedMETs","slimmedMETs") #no MET corr yet
+  metSource = cms.VInputTag("slimmedMETs","slimmedMETs","slimmedMETs","slimmedMETs"), #no MET corr yet
+  lheSource=cms.untracked.InputTag('source')
+
 )
 
 from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
@@ -98,14 +127,14 @@ process.goodOfflinePrimaryVertices = cms.EDFilter(
 #process.kt6PFJets.doRhoFastjet = True
 
 process.p = cms.Path(
-    process.patJets
- + process.patMETs
- + process.inclusiveSecondaryVertexFinderTagInfos
+#    process.patJets
+# + process.patMETs
+# + process.inclusiveSecondaryVertexFinderTagInfos
 #+process.selectedMuons
 #+process.selectedElectrons 
 # +process.goodOfflinePrimaryVertices 
 #+process.kt6PFJets
-+ process.tupel 
+ process.tupel 
 )
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
