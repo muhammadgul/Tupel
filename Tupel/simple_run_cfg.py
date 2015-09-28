31eca90c8787aab13058278fdf123fae8a247a29
Bugra Bilin <bugra.bilin@cern.ch>
2015-09-28 11:59:12 +0200

    update on JEC, added CSVv2, changed jet names

--- Tupel/simple_run_cfg.py
+++ Tupel/simple_run_cfg.py
@@ -1,11 +1,68 @@
 import FWCore.ParameterSet.Config as cms
-
 process = cms.Process("S2")
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring("/store/mc/RunIISpring15DR74/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/00000/0066F143-F8FD-E411-9A0B-D4AE526A0D2E.root")
-)
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
+process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+from JetMETCorrections.Configuration.DefaultJEC_cff import *
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
+runOnData= False #True #data/MC switch
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+#from Configuration.AlCa.GlobalTag import GlobalTag
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+
+if runOnData:
+	process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_v2') #for 25 ns data
+else: 
+	process.GlobalTag = GlobalTag(process.GlobalTag, '74X_mcRun2_asymptotic_v2') #for 25ns mc
+
+process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring("/store/mc/RunIISpring15DR74/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/00000/0066F143-F8FD-E411-9A0B-D4AE526A0D2E.root")
+  fileNames = cms.untracked.vstring("/store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/00000/0AB045B5-BB0C-E511-81FD-0025905A60B8.root")
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+#from CondCore.DBCommon.CondDBSetup_cfi import *
+#import os
+#if runOnData:#
+#	jecfile="Summer15_50nsV4_DATA"
+#else:
+#	jecfile="Summer15_50nsV4_MC"
+
+#dBFile = os.path.expandvars(jecfile+".db")
+#process.jec = cms.ESSource("PoolDBESSource",CondDBSetup,
+#  #connect = cms.string( "sqlite_file://"+dBFile ),
+#  connect = cms.string( "sqlite:"+dBFile),
+#  toGet =  cms.VPSet(
+#  cms.PSet(
+#    record = cms.string("JetCorrectionsRecord"),
+#    tag = cms.string("JetCorrectorParametersCollection_"+jecfile+"_AK4PF"),
+#    label= cms.untracked.string("AK4PF")
+#    ),
+#  cms.PSet(
+#    record = cms.string("JetCorrectionsRecord"),
+#    tag = cms.string("JetCorrectorParametersCollection_"+jecfile+"_AK4PF"),
+#    label= cms.untracked.string("AK4PFchs")
+#    ),
+#  )
+#  )
+#process.es_prefer_jec = cms.ESPrefer("PoolDBESSource",'jec')
+process.load("PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff")
+process.patJetCorrFactorsReapplyJEC = process.patJetCorrFactorsUpdated.clone(
+  src = cms.InputTag("slimmedJets"),
+  levels = ['L1FastJet','L2Relative', 'L3Absolute', 'L2L3Residual'],
+  payload = 'AK4PFchs' ) # Make sure to choose the appropriate levels and payload here!
+
+from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetsUpdated
+process.patJetsReapplyJEC = process.patJetsUpdated.clone(
+  jetSource = cms.InputTag("slimmedJets"),
+  jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
+  )
+process.reapplyJEC = cms.Sequence( process.patJetCorrFactorsReapplyJEC + process.patJetsReapplyJEC)
+########################################
 #from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
 #from RecoJets.JetProducers.ak4PFJets_cfi import ak4PFJets
 #from RecoJets.JetProducers.ak5GenJets_cfi import ak5GenJets
@@ -68,7 +125,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 #process.combinedSecondaryVertex.trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('DYJetsToLL_ntuple.root' )
+                                   fileName = cms.string('withJEC_DYJetsToLL_ntuple.root' )
 )
 
 #process.selectedMuons = cms.EDFilter("CandPtrSelector", src = cms.InputTag("slimmedMuons"), cut = cms.string('''abs(eta)<2.5 && pt>10. &&
@@ -90,7 +147,10 @@ process.TFileService = cms.Service("TFileService",
 #process.patJetPartonMatch.matched = cms.InputTag("prunedGenParticles")
 #process.patJetCorrFactors.primaryVertices=cms.InputTag("offlineSlimmedPrimaryVertices")
 
-
+if runOnData:
+    jetsrcc="slimmedjets"
+else :
+    jetsrcc="patJetsReapplyJEC"
 
 process.tupel = cms.EDAnalyzer("Tupel",
 #  trigger      = cms.InputTag( "patTrigger" ),
@@ -100,7 +160,9 @@ process.tupel = cms.EDAnalyzer("Tupel",
   electronSrc = cms.untracked.InputTag("slimmedElectrons"),
   muonSrc     = cms.untracked.InputTag("slimmedMuons"),
   #tauSrc      = cms.untracked.InputTag("slimmedPatTaus"),
-  jetSrc      = cms.untracked.InputTag("slimmedJets"),
+#  jetSrc      = cms.untracked.InputTag("slimmedJets"),
+
+    jetSrc      = cms.untracked.InputTag("jetsrcc"),
   metSrc      = cms.untracked.InputTag("patMETsPF"),
   genSrc      = cms.untracked.InputTag("prunedGenParticles"),
   gjetSrc       = cms.untracked.InputTag('slimmedGenJets'),
@@ -162,5 +224,4 @@ iFileName = "fileNameDump_cfg.py"
 file = open(iFileName,'w')
 file.write(str(process.dumpPython()))
 file.close()
-
 
